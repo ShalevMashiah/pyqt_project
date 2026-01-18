f104a0711fe48af7d0db4918ab4005ae9c71cd11
Shalev Mashiah <shalev1305@icloud.com>
2026-01-18 14:03:25 +0200

    Add EventBus for decoupled communication

--- infrastructure/events/event_bus.py
+++ infrastructure/events/event_bus.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import QObject, pyqtSignal
 
 class EventBus(QObject):
     #send_counter_signal = pyqtSignal(int)
-
+    send_coordinates_signal = pyqtSignal(int, int)
+    
     def __init__(self):
         super().__init__()
